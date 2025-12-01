149e38134aa8c22db39eb1542f995c867176ac27
wwhhooami <35835444+AutApple@users.noreply.github.com>
2025-12-01 20:31:46 +0300

    fix deep merge so it doesnt affect target object

--- server/src/common/utils/deep-merge-objects.ts
+++ server/src/common/utils/deep-merge-objects.ts
@@ -2,13 +2,17 @@
 export function deepMergeObjects (target: object, merge: object): object {
     if (!merge || !target || typeof(merge) !== 'object' || typeof(target) !== 'object' )
         return target ?? {};
+    
+    // shallow copy the target so its not being modified
+    const output = { ...target };
+    
     for (const key of Object.keys(merge)) {
-        if (!(typeof(merge[key]) === 'object' && typeof(target[key]) === 'object')) {
-            target[key] = merge[key];
+        if (!(typeof(merge[key]) === 'object' && typeof(output[key]) === 'object')) {
+            output[key] = merge[key];
             continue;
         }
-        target[key] = deepMergeObjects(target[key], merge[key]);
+        output[key] = deepMergeObjects(output[key], merge[key]);
     }
-    return target;
+    return output;
 }
 
