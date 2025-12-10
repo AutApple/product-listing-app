de0faf858ec17b17cef4aa14284836c7727fc951
wwhhooami <35835444+AutApple@users.noreply.github.com>
2025-12-10 21:55:29 +0300

    import user entity in user module

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity.js';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([
+    UserEntity
+  ])],
   providers: [UsersService],
+
 })
 export class UsersModule {}
