02f5b9ec7ce2ab2056fd5e3afb33429fee2d00fe
wwhhooami <35835444+AutApple@users.noreply.github.com>
2025-12-17 19:06:30 +0300

    add review image entity decorators

--- server/src/reviews/entities/review-image.entity.ts
+++ server/src/reviews/entities/review-image.entity.ts
@@ -1,9 +1,14 @@
 import { AbstractEntity } from '../../common/entities/abstract.entity.js';
-import { Entity } from 'typeorm';
+import { Column, Entity, ManyToOne } from 'typeorm';
 import { ReviewEntity } from './review.entity.js';
 
 @Entity()
 export class ReviewImageEntity extends AbstractEntity {
+    @ManyToOne(() => ReviewEntity, e => e.images)
     review: ReviewEntity;
+
+    @Column({
+        type: 'varchar'
+    })
     url: string; 
 }
