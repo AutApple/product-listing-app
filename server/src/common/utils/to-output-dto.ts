ee7eab883de17f1d4b8c0dd0a6c787dcdedb30c1
wwhhooami <35835444+AutApple@users.noreply.github.com>
2025-12-18 20:07:33 +0300

    allow OutputDtos constructor  have more than 1 argument

--- server/src/common/utils/to-output-dto.ts
+++ server/src/common/utils/to-output-dto.ts
@@ -1,6 +1,6 @@
 export function toOutputDto<Entity, OutputDto>(
     data: Entity | Entity[], 
-    dtoConstructor: new (entity: Entity) => OutputDto
+    dtoConstructor: new (entity: Entity, ...args: any[]) => OutputDto
 ): OutputDto | OutputDto[] {
     return Array.isArray(data) ? data.map(e => new dtoConstructor(e)) :  new dtoConstructor(data);
 }
