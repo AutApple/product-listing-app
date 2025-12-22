cf6c95382b90fc1313a190b775010b2e29809451
wwhhooami <35835444+AutApple@users.noreply.github.com>
2025-12-22 20:32:05 +0300

    redirect from root to api

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeController } from '@nestjs/swagger';
 
+@ApiExcludeController()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get()
+  @Redirect('/api', 302)
+  redirectToDocs() {}
 }
