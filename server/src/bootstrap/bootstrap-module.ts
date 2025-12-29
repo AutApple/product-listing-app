8b40b6af5664252bd17621810ac3175dfc0ee973
wwhhooami <35835444+AutApple@users.noreply.github.com>
2025-12-29 18:37:33 +0300

    make better identation

--- server/src/bootstrap/bootstrap-module.ts
+++ server/src/bootstrap/bootstrap-module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { RootUserService } from './root-user.service.js';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../users/index.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([
-        UserEntity
-    ])],
+    imports: [
+        TypeOrmModule.forFeature([
+            UserEntity
+        ]), 
+        ConfigModule
+    ],
     providers: [RootUserService]
 }
 )
